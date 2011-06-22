8e345578db84307cee7a4c3224b5d823b1a4fd99
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-06-22 02:36:10 -0300

    Models basicos e configuraçoes basicas

--- minha_grade_fisl/grade/models.py
+++ minha_grade_fisl/grade/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    capacity = models.IntegerField()
+    uid = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+
+class Area(models.Model):
+    description = models.TextField()
+    uid = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+class Author(models.Model):
+    uid = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+class Zone(models.Model):
+    uid = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+class Talk(models.Model):
+    area = models.ForeignKey(Area)
+    room = models.ForeignKey(Room)
+    zone = models.ForeignKey(Zone)
+    hour = models.TimeField()
+    minute = models.TimeField()
+    date = models.DateField()
+    title = models.CharField(max_length=200)
+    abstract = models.TextField()
+    level = models.CharField(max_length=150)
--- minha_grade_fisl/settings.py
+++ minha_grade_fisl/settings.py
@@ -1,4 +1,7 @@
 # Django settings for minha_grade_fisl project.
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, 'minha_grade.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,11 +30,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -56,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static_media')
 )
 
 # List of finder classes that know how to find static files in
@@ -115,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'grade',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
