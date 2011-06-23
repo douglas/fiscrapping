4e632ae66483eda6ed435027d0f4b2e24d2e3496
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-23 00:05:09 -0300

    Updated the views to reflect the models changes

--- minha_grade_fisl/grade/views.py
+++ minha_grade_fisl/grade/views.py
@@ -1,27 +1,37 @@
+# coding: utf-8
+
 from django.utils import simplejson
 from django.http import HttpResponse
 from django.db import IntegrityError
-
+from django.core.exceptions import ObjectDoesNotExist
 from grade.models import Room, Area, Zone, Author, Talk
+
 
 def gerar_rooms(json):
     for room in json['rooms']:
-        Room.objects.create(uid=room['room_id'], capacity=room['capacity'], name=room['name'])
+        Room.objects.create(uid=room['room_id'],
+                            capacity=room['capacity'],
+                            name=room['name'],
+                            translation=room['translation'],
+                            position=room['position'])
+
 
 def gerar_areas(json):
     for area in json['areas']:
         Area.objects.create(uid=area['area_id'], name=area['name'])
 
+
 def gerar_zones(json):
     for zone in json['zones']:
         Zone.objects.create(uid=zone['zone_id'], name=zone['name'])
 
+
 def gerar_authors(json):
     for author in json['authors']:
-        try:
-            Author.objects.create(uid=author['candidate'], name=author['name'])
-        except IntegrityError:
-            pass
+        Author.objects.create(uid=author['author_id'],
+                              candidate=author['candidate'],
+                              name=author['name'])
+
 
 def gerar_talks(json):
     gerar_areas(json)
@@ -30,19 +40,25 @@ def gerar_talks(json):
     gerar_authors(json)
 
     for talk in json['talks']:
-        try:
-            Talk.objects.get_or_create(area=Area.objects.get(uid=talk['area_id']),
-                                   room=Room.objects.get(uid=talk['room_id']),
-                                   zone=Zone.objects.get(uid=talk['zone_id']),
-                                   author=Author.objects.get(uid=talk['candidate']),
-                                   level=talk['level'],
-                                   hour=talk['hour'],
-                                   minute=talk['minute'],
-                                   date=talk['date'],
-                                   abstract=talk['abstract'],
-                                   title=talk['title'])
-        except IntegrityError:
-            pass
+        t = Talk(area=Area.objects.get(uid=talk['area_id']),
+                 room=Room.objects.get(uid=talk['room_id']),
+                 zone=Zone.objects.get(uid=talk['zone_id']),
+                 level=talk['level'],
+                 hour=talk['hour'],
+                 minute=talk['minute'],
+                 date=talk['date'],
+                 abstract=talk['abstract'],
+                 title=talk['title'])
+
+        # To get the id
+        t.save()
+
+        # Now we tie the authors to the talk
+        t.authors = Author.objects.filter(candidate=talk['candidate'])
+        t.save()
+    
+    import ipdb; ipdb.set_trace()
+
 
 def gerar_grade(request):
     data_json = open("static_media/json/data.json", "r").read()
