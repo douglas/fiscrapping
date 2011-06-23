70fe5217cc1ad3d09dd6fb6e7231473729e5c5d5
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-23 00:04:23 -0300

    Updated the models to the new constraints

--- minha_grade_fisl/grade/models.py
+++ minha_grade_fisl/grade/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Room(models.Model):
     capacity = models.IntegerField()
     uid = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    translation = models.BooleanField()
 
 
 class Area(models.Model):
@@ -11,22 +14,26 @@ class Area(models.Model):
     uid = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
 
+
 class Author(models.Model):
-    uid = models.IntegerField(unique=True)
+    uid = models.IntegerField()
     name = models.CharField(max_length=200)
+    candidate = models.IntegerField()
+
 
 class Zone(models.Model):
     uid = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
 
+
 class Talk(models.Model):
     area = models.ForeignKey(Area)
     room = models.ForeignKey(Room)
     zone = models.ForeignKey(Zone)
-    author = models.ForeignKey(Author)
     hour = models.CharField(max_length=2)
-    minute = models.CharField(max_length=2)
-    date = models.DateField()
     title = models.CharField(max_length=200)
+    date = models.DateField()
     abstract = models.TextField()
     level = models.CharField(max_length=150)
+    authors = models.ManyToManyField('Author')
+    minute = models.CharField(max_length=2)
