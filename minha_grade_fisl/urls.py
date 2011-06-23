0207a79e5ecd7a3173fd95a7dba82ef51e102a71
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-23 02:09:44 -0300

    Updated urls.py to include grade app urls and including django-dt-htmltidy urls

--- minha_grade_fisl/urls.py
+++ minha_grade_fisl/urls.py
@@ -1,24 +1,25 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
+
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'grade.views.home', name='home'),
-    # url(r'^minha_grade_fisl/', include('minha_grade_fisl.foo.urls')),
+    (r'^', include('grade.urls', namespace='grade')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r"^gerar_grade/", 'grade.views.gerar_grade', name="gerar_grade"),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.STATIC_ROOT}),
-                       
 )
 
 urlpatterns += staticfiles_urlpatterns()
+
+if 'debug_toolbar_htmltidy' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+        url(r'^', include('debug_toolbar_htmltidy.urls')),
+    )
