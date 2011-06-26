9eb2683597f041a75d3d8b417a78ba0661ddc234
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-26 18:00:59 -0300

    Adding some palestra views

--- minha_grade_fisl/grade/urls.py
+++ minha_grade_fisl/grade/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls.defaults import patterns, url
+from views import TalkListView, TalkDetailView
 
-urlpatterns = patterns('',
-    url(r'^$', 'grade.views.home', name='home'),
-    url(r"^gerar_grade/", 'grade.views.gerar_grade', name="gerar_grade"),
+urlpatterns = patterns('grade.views',
+    url(r'^palestras/$', TalkListView.as_view(), name='talks'),
+    url(r'^palestras/(?P<pk>\d+)/$', TalkDetailView.as_view(),
+        name='talk'),
+
+    url(r'^$', 'home', name='home'),
+    url(r"^gerar_grade/", 'gerar_grade', name="gerar_grade"),
 )
