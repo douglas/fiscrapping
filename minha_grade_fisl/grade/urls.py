c6fc48b66c47acfc409884b679a57bd3baf49b18
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-27 03:09:31 -0300

    The default is to go to the talks (palestras) page

--- minha_grade_fisl/grade/urls.py
+++ minha_grade_fisl/grade/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls.defaults import patterns, url
 from views import TalkListView, TalkDetailView
 
 urlpatterns = patterns('grade.views',
-    url(r'^palestras/$', TalkListView.as_view(), name='talks'),
+    url(r'^$', TalkListView.as_view(), name='talks'),
     url(r'^palestras/(?P<pk>\d+)/$', TalkDetailView.as_view(),
         name='talk'),
 
-    url(r'^$', 'home', name='home'),
     url(r"^gerar_grade/", 'gerar_grade', name="gerar_grade"),
 )
