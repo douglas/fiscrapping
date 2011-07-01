87cf6694565cd7936420fad69abc60da24087590
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-07-01 16:57:09 -0300

    Thanks for the guys at globo.com for making me enter in the testing mood, in this commit all tests pass =) (also, chuck norris is seems to be happy now)

--- minha_grade_fisl/grade/tests.py
+++ minha_grade_fisl/grade/tests.py
@@ -4,37 +4,64 @@ from django.utils import simplejson
 
 from grade.views import gerar_rooms, gerar_areas, gerar_zones, gerar_authors
 from grade.views import gerar_talks
-from grade.views import home
+from grade.views import clean_data
 
 from grade.models import Room, Area, Zone, Author, Talk
 
 
 class TestViews(TestCase):
     def setUp(self):
+        clean_data()
+
         data_json = open("public/json/data.json", "r").read()
         self.json = simplejson.loads(data_json)
 
         self.factory = RequestFactory()
 
+    def test_roomset_is_empty(self):
+        clean_data()
+
+        self.assertEquals(Room.objects.count(), 0)
+
+    def test_areaset_is_empty(self):
+        clean_data()
+
+        self.assertEquals(Area.objects.count(), 0)
+
+    def test_zoneset_is_empty(self):
+        clean_data()
+
+        self.assertEquals(Zone.objects.count(), 0)
+
+    def test_authorset_is_empty(self):
+        clean_data()
+
+        self.assertEquals(Author.objects.count(), 0)
+
+    def test_talkset_is_empty(self):
+        clean_data()
+
+        self.assertEquals(Talk.objects.count(), 0)
+
     def test_generate_room_from_json(self):
         gerar_rooms(self.json)
-        self.assertEquals(len(Room.objects.all()), 12)
+        self.assertEquals(Room.objects.count(), 12)
 
     def test_generate_areas_from_json(self):
         gerar_areas(self.json)
-        self.assertEquals(len(Area.objects.all()), 22)
+        self.assertEquals(Area.objects.count(), 22)
 
     def test_genereta_zone_from_json(self):
         gerar_zones(self.json)
-        self.assertEquals(len(Zone.objects.all()), 7)
+        self.assertEquals(Zone.objects.count(), 7)
 
     def test_generate_author_from_json(self):
         gerar_authors(self.json)
-        self.assertEquals(len(Author.objects.all()), 495)
+        self.assertEquals(Author.objects.count(), 508)
 
     def test_generate_talk_from_json(self):
         gerar_talks(self.json)
-        self.assertEquals(len(Talk.objects.all()), 351)
+        self.assertEquals(Talk.objects.count(), 352)
 
     def test_access_view_and_generate_talks(self):
         client = Client()
@@ -44,11 +71,9 @@ class TestViews(TestCase):
         self.assertEquals(response.content, "Grade gerada com sucesso!")
 
     def test_access_home_and_see_all_talks(self):
-        request = self.factory.get('/')
+        # TODO: Use splinter to test browser access
 
-        gerar_talks(self.json)
-        response = home(request)
+        client = Client()
+        response = client.get('/')
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(response.context_data['talks']),
-                          len(Talk.objects.all()))
