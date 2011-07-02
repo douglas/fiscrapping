27f44f232f8bec562cfc169f7805c6cde64aa9da
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-07-02 07:48:21 -0300

    Added an user for testing the choice_talk method (which was added now)

--- minha_grade_fisl/grade/tests.py
+++ minha_grade_fisl/grade/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from django.test.client import RequestFactory
 from django.utils import simplejson
+from django.core.urlresolvers import reverse
 
 from grade.views import gerar_rooms, gerar_areas, gerar_zones, gerar_authors
-from grade.views import gerar_talks
-from grade.views import clean_data
+from grade.views import gerar_talks, clean_data
 
 from grade.models import Room, Area, Zone, Author, Talk
+
+from django.contrib.auth.models import User
+from twitterauth.models import Profile
 
 
 class TestViews(TestCase):
@@ -15,6 +18,17 @@ class TestViews(TestCase):
         self.json = simplejson.loads(data_json)
 
         self.factory = RequestFactory()
+
+        # we need an user
+        user = User.objects.create_user('gradefisl',
+                                'gradefisl@twitter.com',
+                                'QZAx8IwvwpLOHGNDOTrIhSO3E5KyeOtb9UIrqJ9nyM')
+
+        profile = Profile()
+        profile.user = user
+        profile.oauth_token = '324585531-Q1syPHxVC8X0ausLadVHDnDhzXRxx0z26lRjuHBm'
+        profile.oauth_secret = 'QZAx8IwvwpLOHGNDOTrIhSO3E5KyeOtb9UIrqJ9nyM'
+        profile.save()
 
     def test_roomset_is_empty(self):
         clean_data()
@@ -60,6 +74,20 @@ class TestViews(TestCase):
     def test_generate_talk_from_json(self):
         gerar_talks(self.json)
         self.assertEquals(Talk.objects.count(), 362)
+
+    def test_choice_talk(self):
+        talk_id = 10
+        talk = Talk.objects.get(id=talk_id)
+        user = User.objects.get(id=1)
+
+        client = Client()
+        client.login(username='gradefisl',
+                     password='QZAx8IwvwpLOHGNDOTrIhSO3E5KyeOtb9UIrqJ9nyM')
+
+        # lets choice the talk
+        client.get(reverse('grade:choice_talk', args=[10]))
+
+        self.assertEquals(talk.listeners.filter(id=user.id).count(), 1)
 
     def test_access_view_and_generate_talks(self):
         client = Client()
