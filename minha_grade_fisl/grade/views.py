b106df6b0fd7c8092ce253136b2325926470f3d9
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-07-02 05:54:07 -0300

    Added some code to clear data in the grade app related tables and also fixed all the issues when adding data when the database is already populated (from the fixture)

--- minha_grade_fisl/grade/views.py
+++ minha_grade_fisl/grade/views.py
@@ -78,28 +78,32 @@ def choice_talk(request, talk_id):
 
 def gerar_rooms(json):
     for room in json['rooms']:
-        Room.objects.create(uid=room['room_id'],
-                            capacity=room['capacity'],
-                            name=room['name'],
-                            translation=room['translation'],
-                            position=room['position'])
+        if not Room.objects.filter(uid=room['room_id']).count():
+            Room.objects.create(uid=room['room_id'],
+                                capacity=room['capacity'],
+                                name=room['name'],
+                                translation=room['translation'],
+                                position=room['position'])
 
 
 def gerar_areas(json):
     for area in json['areas']:
-        Area.objects.create(uid=area['area_id'], name=area['name'])
+        if not Area.objects.filter(uid=area['area_id']).count():
+            Area.objects.create(uid=area['area_id'], name=area['name'])
 
 
 def gerar_zones(json):
     for zone in json['zones']:
-        Zone.objects.create(uid=zone['zone_id'], name=zone['name'])
+        if not Zone.objects.filter(uid=zone['zone_id']).count():
+            Zone.objects.create(uid=zone['zone_id'], name=zone['name'])
 
 
 def gerar_authors(json):
     for author in json['authors']:
-        Author.objects.create(uid=author['author_id'],
-                              candidate=author['candidate'],
-                              name=author['name'])
+        if not Author.objects.filter(uid=author['author_id']).count():
+            Author.objects.create(uid=author['author_id'],
+                                  candidate=author['candidate'],
+                                  name=author['name'])
 
 
 def gerar_talks(json):
@@ -109,22 +113,23 @@ def gerar_talks(json):
     gerar_authors(json)
 
     for talk in json['talks']:
-        t = Talk(area=Area.objects.get(uid=talk['area_id']),
-                 room=Room.objects.get(uid=talk['room_id']),
-                 zone=Zone.objects.get(uid=talk['zone_id']),
-                 level=talk['level'],
-                 hour=talk['hour'],
-                 minute=talk['minute'],
-                 date=talk['date'],
-                 abstract=talk['abstract'],
-                 title=talk['title'])
+        if not Talk.objects.filter(area=talk['area_id']).count():
+            t = Talk(area=Area.objects.get(uid=talk['area_id']),
+                     room=Room.objects.get(uid=talk['room_id']),
+                     zone=Zone.objects.get(uid=talk['zone_id']),
+                     level=talk['level'],
+                     hour=talk['hour'],
+                     minute=talk['minute'],
+                     date=talk['date'],
+                     abstract=talk['abstract'],
+                     title=talk['title'])
 
-        # To get the id
-        t.save()
+            # To get the id
+            t.save()
 
-        # Now we tie the authors to the talk
-        t.authors = Author.objects.filter(candidate=talk['candidate'])
-        t.save()
+            # Now we tie the authors to the talk
+            t.authors = Author.objects.filter(candidate=talk['candidate'])
+            t.save()
 
 
 def gerar_grade(request):
